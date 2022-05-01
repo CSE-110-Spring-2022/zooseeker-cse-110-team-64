7e0c4e1f7b7bd09ede17532ebd9738ee5e231a6f
knguyen33 <kevin.nguyen@parleylabs.com>
2022-05-01 16:17:16 -0700

    Updated SearchBar UI

--- app/src/main/java/com/example/sdzooseeker_team_64/MainActivity.java
+++ app/src/main/java/com/example/sdzooseeker_team_64/MainActivity.java
@@ -1,18 +1,59 @@
 package com.example.sdzooseeker_team_64;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+
+    String[] sample = {"Gorilla", "Dog", "Kangaroo", "Snake", "Sneasel"}; //etc.
+
+    ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, SearchPage.class);
-        startActivity(intent);
+        listView = findViewById(R.id.list);
+
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, sample);
+        listView.setAdapter(arrayAdapter);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu, menu);
+
+        MenuItem menuItem = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setQueryHint("Animal Name");
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                arrayAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
