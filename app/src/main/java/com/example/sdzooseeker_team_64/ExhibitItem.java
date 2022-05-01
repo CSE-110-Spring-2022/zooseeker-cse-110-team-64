f95ec71f5cdb8f267e234c5ae7db8b0d7db85a97
Tech Kern Ding <tkding@ucsd.edu>
2022-05-01 13:14:29 -0700

    search bar UI

---
+++ app/src/main/java/com/example/sdzooseeker_team_64/ExhibitItem.java
@@ -0,0 +1,34 @@
+package com.example.sdzooseeker_team_64;
+
+import android.content.Context;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.lang.reflect.Type;
+import java.util.Collections;
+import java.util.List;
+
+public class ExhibitItem {
+    // 1. public fields
+
+    // 2. Constructor matching field above.
+
+    //3. Factory method for loading our JSON.
+    public static List<ExhibitItem> loadJSON(Context context, String path) {
+        try {
+            InputStream input = context.getAssets().open(path);
+            Reader reader = new InputStreamReader(input);
+            Gson gson = new Gson();
+            Type type = new TypeToken<List<ExhibitItem>>() {}.getType(); // anonymous class syntax
+            return gson.fromJson(reader, type);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return Collections.emptyList();
+        }
+    }
+}
