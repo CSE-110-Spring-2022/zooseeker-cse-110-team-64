d0e89dff71826048df91a4422514c60232eef281
marincao <marincao626@gmail.com>
2022-05-05 22:10:25 -0700

    list_us2

--- app/src/main/java/com/example/sdzooseeker_team_64/MainActivity.java
+++ app/src/main/java/com/example/sdzooseeker_team_64/MainActivity.java
@@ -2,21 +2,32 @@ package com.example.sdzooseeker_team_64;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    //new:
+    ArrayList<String> exhibitList = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
+    ListView newlist;
+    //end
     ListView listView;
-
     String[] sample = {"Gorilla", "Dog", "Kangaroo", "Snake", "Sneasel"}; //etc.
-
     ArrayAdapter<String> arrayAdapter;
 
     @Override
@@ -25,10 +36,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         listView = findViewById(R.id.list);
-
         arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, sample);
         listView.setAdapter(arrayAdapter);
 
+        // new:
+        newlist = findViewById(R.id.new_list);
+        adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                exhibitList);
+        newlist.setAdapter(adapter);
+
+        // end:
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String item = (String)adapterView.getItemAtPosition(i);
+                if(exhibitList.contains(item) == true) {
+                    return;
+                }
+                exhibitList.add(item);
+                adapter.notifyDataSetChanged();
+            }
+        });
     }
 
     @Override
@@ -51,10 +80,16 @@ public class MainActivity extends AppCompatActivity {
             public boolean onQueryTextChange(String newText) {
 
                 arrayAdapter.getFilter().filter(newText);
+
                 return false;
             }
+
         });
+
 
         return super.onCreateOptionsMenu(menu);
     }
+
+
+
 }
