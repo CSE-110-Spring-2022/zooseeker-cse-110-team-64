331201735216ce1a1ffac4ad31a839d08e22863b
Tech Kern Ding <tkding@ucsd.edu>
2022-05-05 22:23:31 -0700

    navigation UI

---
+++ app/src/main/java/com/example/sdzooseeker_team_64/navigation_page.java
@@ -0,0 +1,14 @@
+package com.example.sdzooseeker_team_64;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class navigation_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation_page);
+    }
+}
